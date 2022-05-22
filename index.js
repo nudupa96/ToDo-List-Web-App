136a3f97d546679e856610ee29db397f2538aaba
Nagendra K Udupa <nagudupa@Nagendras-MacBook-Pro.local>
2022-05-22 15:43:26 +0530

    Installed EJS and Setup View Engine and link with Views

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+//Setup the view engine (name and value pair)
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.listen(port, function(err){
     if(err)
     {
