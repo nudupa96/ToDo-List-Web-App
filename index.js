85a3ce8bc71d0b8633fe83ebfbb199e129f6823a
Nagendra K Udupa <nagudupa@Nagendras-MacBook-Pro.local>
2022-05-22 17:22:49 +0530

    Installed EJS Express layouts, edited entry point to use router, linked static files, used layouts, added layout properties of styles and scripts to true, created required EJS files(layout,home,update,_header and _footer)

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+//import the express-ejs-layouts lib
+const expressLayouts = require('express-ejs-layouts');
+
+//link the static files folder called assets
+app.use(express.static('./assets'));
+
+//need to use ejs layouts lib before routes  is required
+app.use(expressLayouts);
+
+//extract styles and scripts from sub pages into the layout
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
+
+//use the routes here and access the routes folder
+app.use('/', require('./routes'));
 
 //Setup the view engine (name and value pair)
 app.set('view engine', 'ejs');
