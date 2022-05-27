eafa6f1c40183a7e006631e87e547967535c3f10
Nagendra K Udupa <nagudupa@Nagendras-MacBook-Pro.local>
2022-05-27 21:51:07 +0530

    Added jQuery script, Added AJAX call in delete_script.js script

---
+++ assets/js/delete_script.js
@@ -0,0 +1,40 @@
+//this function is called whenever delete operation needs to be performed
+function performDeleteOperation() {
+    //first get al the checked input to be deleted
+    let checkedBoxes = document.querySelectorAll('input[name="taskIDs"]:checked');
+    //now create an empty array to be operated on
+    let IDs = [];
+
+    //add the values to IDs array
+    checkedBoxes.forEach(function (box){
+        IDs.push(box.value);
+    });
+
+    //AJAX cal through the server - Note the syntax
+    $.ajax({
+        method: 'POST',
+        url: '/delete-task',
+        data: {
+            taskIDs: IDs,
+        },
+        success: (data) => {
+            //this console is on the web
+            console.log("Deleted Content", data);
+            taskIDs = data.taskIDs;
+
+            //if there is an array(object in JS) of tasks to be deleted
+            if( typeof taskIDs == 'object') {
+                taskIDs.forEach((task,index) => {
+                    //remove this value
+                    $(`#task-${task}`).remove();
+                })
+            }
+            else {
+                $(`#task-${taskIDs}`).remove();
+            }
+        },
+        error: (error) => {
+            console.log(`Error in deleting tasks: ${error}`);
+        }
+    })
+}
